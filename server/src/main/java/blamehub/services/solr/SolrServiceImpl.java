791081e495e462597a4f0bcd2a52ebfbdd322444
Dviri_Yigal <yigal.dviri@ca.com>
2016-08-19 13:38:42 +0300

    Solr code styling and moving options to properties file

--- server/src/main/java/blamehub/services/solr/SolrServiceImpl.java
+++ server/src/main/java/blamehub/services/solr/SolrServiceImpl.java
@@ -8,9 +8,11 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.GroupCommand;
 import org.apache.solr.client.solrj.response.GroupResponse;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.Date;
@@ -26,6 +28,10 @@ public class SolrServiceImpl implements SolrService{
     @Inject
     private SolrClient solrClient;
 
+    @Resource
+    private Environment environment;
+
+
     @Override
     public void addCommitDocs(List<CommitDoc> commitDocs) {
         if (commitDocs != null && !commitDocs.isEmpty()){
@@ -36,19 +42,45 @@ public class SolrServiceImpl implements SolrService{
     // TODO: Move to use Spring as well
     @Override
     public List<GroupCommand> findByTerms(String terms) throws IOException, SolrServerException {
+        SolrQuery solrQuery = createBoostedQueryForTerms(terms);
+        setQueryFilters(solrQuery);
+        setGrouping(solrQuery);
+        setNumOfResults(solrQuery);
+
+        return executeQuery(solrQuery);
+    }
+
+    private List<GroupCommand> executeQuery(SolrQuery solrQuery) throws SolrServerException, IOException {
+        QueryResponse query = solrClient.query(solrQuery);
+        GroupResponse results = query.getGroupResponse();
+        return results.getValues();
+    }
+
+    private void setQueryFilters(SolrQuery solrQuery) {
+        String timeBack = environment.getRequiredProperty("solr.time.go.back");
+
+        // Want to filter commits of merged branches and pull requests.
+        solrQuery.set("fq", "date_dt:{" + timeBack + "}  AND -message:Merge*");
+    }
+
+    private void setGrouping(SolrQuery solrQuery) {
+        String groupLimit = environment.getRequiredProperty("solr.group.limit");
+        solrQuery.set("group", true);
+        solrQuery.set("group.field", "name_txt_en");
+        solrQuery.set("group.limit", groupLimit);
+    }
+
+    private void setNumOfResults(SolrQuery solrQuery) {
+        String resultsNum = environment.getRequiredProperty("solr.num.results");
+        solrQuery.set("rows", resultsNum);
+    }
+
+    private SolrQuery createBoostedQueryForTerms(String terms) {
         SolrQuery solrQuery = new  SolrQuery().setQuery(terms);
         solrQuery.set("defType","edismax");
         solrQuery.set("fl","*,score");
         solrQuery.set("bf","recip(ms(NOW/DAY,date_dt),3.16e-11,1,1)");
-        solrQuery.set("fq","date_dt:{NOW/DAY-2YEARS TO NOW/DAY+1DAY}");
-        solrQuery.set("group", true);
-        solrQuery.set("group.field", "name_txt_en");
-        solrQuery.set("group.limit", 3);
-        solrQuery.set("rows", 3);
-        QueryResponse query = solrClient.query(solrQuery);
-
-        GroupResponse results = query.getGroupResponse();
-        return results.getValues();
+        return solrQuery;
     }
 
 
